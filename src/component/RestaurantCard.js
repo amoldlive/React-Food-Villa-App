348e5ba9065b23fe7315249448ca15e975dcf33f
Amol Dhawale <32627412+amoldlive@users.noreply.github.com>
2023-04-13 11:29:58 +0530

    hover border updated

--- src/component/RestaurantCard.js
+++ src/component/RestaurantCard.js
@@ -7,7 +7,7 @@ const RestaurantCard = ({
   area,
 }) => {
   return (
-    <div className="w-60 h-64 p-5 drop-shadow-lg border-solid border-1 border-red-50 m-5">
+    <div className="w-60 h-64 p-5 drop-shadow-lg border-solid border-1 border-red-50 m-3 hover:border-2">
       <img className="h-20 w-60" alt="food image" src={productImgApi + cloudinaryImageId} />
       <h5 className="font-semibold text-base">{name}</h5>
       <p className="bg-green-200 w-10 rounded-md mb-2">* {avgRating}</p>
